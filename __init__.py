262201cbb17b85a78d7a9ebc57be15dd1ab237f6
BhavikKatyal <40070017+BhavikKatyal@users.noreply.github.com>
2019-02-25 14:50:58 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -14,75 +14,38 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
-from __future__ import print_function
+import requests
 from adapt.intent import IntentBuilder
 
 from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
 
-import datetime
-import pickle
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
 __author__ = 'Bhavik_Katyal'
 
 LOGGER = getLogger(__name__)
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
 
 class NewSkill(MycroftSkill):
     def __init__(self):
         super(NewSkill, self).__init__(name="NewSkill")
 
     def initialize(self):
-        get_event_intent = IntentBuilder("GetEventIntent"). \
-            require("GetEventIntentKeyword").build()
-        self.register_intent(get_event_intent, self.handle_get_event_intent)
+
+        cal_event_intent = IntentBuilder("CalEventIntent"). \
+            require("CalEventKeyword").build()
+        self.register_intent(cal_event_intent, self.handle_cal_event_intent)
 
 
-    def handle_get_event_intent(self, message):
-        creds = None
-        # The file token.pickle stores the user's access and refresh tokens, and is
-        # created automatically when the authorization flow completes for the first
-        # time.
-        if os.path.exists('token.pickle'):
-            with open('token.pickle', 'rb') as token:
-                creds = pickle.load(token)
-        # If there are no (valid) credentials available, let the user log in.
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    'credentials.json', SCOPES)
-                creds = flow.run_local_server()
-            # Save the credentials for the next run
-            with open('token.pickle', 'wb') as token:
-                pickle.dump(creds, token)
 
-        service = build('calendar', 'v3', credentials=creds)
+        
 
-        # Call the Calendar API
-        now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-        print('Getting the upcoming 10 events')
-        events_result = service.events().list(calendarId='primary', timeMin=now,
-                                            maxResults=10, singleEvents=True,
-                                            orderBy='startTime').execute()
-        events = events_result.get('items', [])
+    def handle_cal_event_intent(self, message):
 
-        if not events:
-            print('No upcoming events found.')
-        for event in events:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            print(start, event['summary'])
-        self.speak_dialog("how are you")
+        self.speak_dialog("Did I forget to introduce myself? I’m your voice Assistant. Maverick!!”)
 
+    
 
 
     def stop(self):
         pass
 
-
-def create_skill():
-    return NewSkill()
